1d28f5c556127056996b47f7ee014cb691a459c9
andreza vilar <andreza.farias@ccc.ufcg.edu.br>
2024-06-05 14:43:27 -0300

    lendo e criando arquivos

---
+++ ManipulandoArquivosExercicios/src/main/java/entities/Product.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Product {
+    private String productName;
+    private Integer quantity;
+    private Double price;
+
+    public Product(String productName, int quantity, double price) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double total(){
+        return quantity*price;
+    }
+
+
+}
